4800acc9280128525d4acbf8e1331a2631fc95a7
mortezaa <mortezajafari76@gmail.com>
2025-09-14 11:27:25 +0330

    fix: provider issue

--- src/StoriesServiceProvider.php
+++ src/StoriesServiceProvider.php
@@ -4,57 +4,45 @@ declare(strict_types=1);
 
 namespace Mortezaa97\Stories;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Mortezaa97\Stories\Models\Story;
+use Mortezaa97\Stories\Policies\StoryPolicy;
 
 class StoriesServiceProvider extends ServiceProvider
 {
     /**
-     * Bootstrap the application services.
+     * Bootstrap package services.
      */
-    public function boot()
+    public function boot(): void
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'stories');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'stories');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+        // Load migrations
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        // Load routes
+        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+
+        // Register policies
+        Gate::policy(Story::class, StoryPolicy::class);
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__ . '/../config/config.php' => config_path('stories.php'),
             ], 'config');
 
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/stories'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/stories'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/stories'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
+            $this->publishes([
+                __DIR__ . '/../database/migrations' => database_path('migrations'),
+            ], 'migrations');
         }
     }
 
     /**
-     * Register the application services.
+     * Register package services.
      */
-    public function register()
+    public function register(): void
     {
-        // Automatically apply the package configuration
         $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'stories');
 
-        // Register the main class to use with the facade
         $this->app->singleton('stories', function () {
             return new Stories;
         });
