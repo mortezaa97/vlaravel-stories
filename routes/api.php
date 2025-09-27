79d53386237436abc202708e8c2660abfe651412
mortezaa <mortezajafari76@gmail.com>
2025-09-27 13:13:38 +0330

    fix:route api middleware issue

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Mortezaa97\Stories\Http\Controllers\StoryController;
 
-Route::prefix('api/stories')->group(function () {
+Route::prefix('api/stories')->middleware('api')->group(function () {
     Route::get('/', [StoryController::class, 'index'])->name('stories.index');
     Route::get('/{story:slug}', [StoryController::class, 'show'])->name('stories.show');
 });
